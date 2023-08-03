a351f724496a9c334651ee0ec7ad222ef70770dc
Sarkar <sarkerakash662@gmail.com>
2023-08-03 16:08:42 +0600

    first commit

---
+++ hello.dart
@@ -0,0 +1,24 @@
+void main() {
+  const int a = 19;
+  const int b = 4;
+
+  /// Addition
+  int addition = a + b;
+  print(addition);
+
+  ///Subtraction
+  int subtraction = a - b;
+  print(subtraction);
+
+  ///Multiplication
+  int multiplication = a * b;
+  print(multiplication);
+
+  ///Division
+  double division = a / b;
+  print(division);
+
+  ///Modulus
+  int modulus = a % b;
+  print(modulus);
+}
